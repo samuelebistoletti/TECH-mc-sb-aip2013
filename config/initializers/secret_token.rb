56724bc2ed7ae907b7352453d81ac15e59a0ff3a
samuelebistoletti <samuele.bistoletti@gmail.com>
2013-06-06 15:56:40 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TECHMcSbAip2013::Application.config.secret_key_base = 'dd8aba5027e75e5f59d242b53a0717ecffa990c71e3ee4f3db939cb8d74446a2f7fb0bae2b3ec9fd41f54a6d0244a4bd859a173ccf8d258c55ea94bf0374ae8c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TECHMcSbAip2013::Application.config.session_store :cookie_store, key: '_TECH-mc-sb-aip2013_session'
