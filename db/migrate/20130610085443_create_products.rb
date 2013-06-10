95664b2bb59ec9104a00e4eebcb5ec7738b8a8f0
Marco Cavallo <mastercava@hotmail.it>
2013-06-10 10:59:51 +0200

    Db

---
+++ db/migrate/20130610085443_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.string :description
+      t.string :category
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
