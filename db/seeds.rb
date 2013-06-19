6438f7043c56f438934493b58a8213a05abedfb8
Marco Cavallo <mastercava@hotmail.it>
2013-06-19 15:12:45 +0200

    Db

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,10 +11,15 @@ Product.create([{name: 'Graduate',
                  description: 'Scaffale dal design moderno e raffinato, adatto per soprammobili, libri e hi-fi. Si adatta a qualsiasi tipo di parete e ambiente, il suo design fine lo rende ideale per ogni casa moderna.',
                  image_url: 'armadio.jpg',
                  specifications: 'Piano di sostegno: alluminio anodizzato con bordo in multistrati<br/>Ripiani: alluminio anodizzato con bordo in multistrati<br/>Tiranti: acciaio inox</br>Portata massima delle librerie.<br/>Libreria L 2000 Kg 240<br/>Libreria L 3000 Kg 360<br/>Portata max. singolo ripiano Kg 25',
-                 designer: '1'},
+                 designer: '1',
+                 category: 'Scaffali'
+                },
 
                 {name: 'Landscape',
                  description: 'Serie di imbottiti fissi e componibili caratterizzati dalla seduta sporgente rispetto alla struttura. Questo aspetto si evidenzia maggiormente nelle composizioni, quando elemento termin',
                  image_url: 'divano.jpg',
                  specifications: 'Interno struttura<br/>Telaio composto da legno massello di Abete e pannelli di particelle di legno, con cinghie elastiche.<br/><br/>Imbottitura struttura<br/>Poliuretano espanso ad alta portanza a densita variabile.<br/>Fodera in vellutino accoppiato.',
-                 designer: '1'}])
+                 designer: '1',
+                 category: 'Divani'
+                }
+               ])
