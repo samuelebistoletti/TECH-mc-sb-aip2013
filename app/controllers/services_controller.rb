1e21dcc42b49f9027de77204a9a7af7931653d7f
samuelebistoletti <samuele.bistoletti@gmail.com>
2013-06-19 19:19:53 +0200

    CSS

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -7,6 +7,7 @@ class ServicesController < ApplicationController
     @generalServices = Service.find_all_by_service_type(0)
     @specificServices = Service.find_all_by_service_type(1)
 
+
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @services }
