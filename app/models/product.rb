9de93b34c1b59a60f906b6f28224e024db9e6dcf
Marco Cavallo <mastercava@hotmail.it>
2013-06-20 10:48:38 +0200

    Db

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,6 @@
 class Product < ActiveRecord::Base
-  attr_accessible :name, :description, :image_url, :specifications, :designer, :category
+  attr_accessible :name, :description, :image_url, :specifications, :designer_id, :category_id
   has_and_belongs_to_many :services, association_foreign_key: 'service_id'
   belongs_to :category, :foreign_key => :category_id
   belongs_to :designer, :foreign_key => :designer_id
-  #belongs_to :customer, foreign_key: 'designer'
 end
