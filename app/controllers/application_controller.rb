652df86fd2a2329cf051d9636cd584ddcc1b536a
Marco Cavallo <mastercava@hotmail.it>
2013-06-21 13:11:58 +0200

    Db

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  rescue_from Exception, :with => :render_error
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_error
+  rescue_from ActionController::RoutingError, :with => :render_error
+  rescue_from ActionController::UnknownController, :with => :render_error
+  rescue_from ActionController::UnknownAction, :with => :render_error
+
+  def render_error
+    render :template => 'errors/error'
+  end
 end
